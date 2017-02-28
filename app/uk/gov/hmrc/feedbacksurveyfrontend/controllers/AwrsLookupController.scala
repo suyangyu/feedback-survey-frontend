be089294e0b18626bb2180fe64bdb4d417eef5e1
richy <richy.jassal@accenture.com>
2017-02-28 09:22:30 +0000

    initial commit

---
+++ app/uk/gov/hmrc/feedbacksurveyfrontend/controllers/AwrsLookupController.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import models.awrsModels._
+import uk.gov.hmrc.play.frontend.auth.AuthContext
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import play.api.mvc._
+import uk.gov.hmrc.play.http.HeaderCarrier
+import scala.concurrent.Future
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+
+import scala.util.{Failure, Success, Try}
+
+
+object AwrsLookupController extends AwrsLookupController
+
+trait AwrsLookupController extends FrontendController {
+
+  val page1 = Action.async { implicit request =>
+    Future.successful(Ok(uk.gov.hmrc.feedbacksurveyfrontend.views.html.awrsLookup.page1(formMappings.page1Form)))
+  }
+
+  val page1Continue = Action(parse.form(formMappings.page1Form)) { implicit request =>
+    Redirect(routes.AwrsLookupController.page2())
+  }
+
+//  val page1Continue: Future[Result] = {
+//    formMappings.page1Form.bindFromRequest.fold(
+//      errors => {
+//        Future.successful(Ok(uk.gov.hmrc.feedbacksurveyfrontend.views.html.awrsLookup.page1(errors)))
+//      },
+//      formData => {
+//        Redirect(controllers.routes.AwrsLookupController.page2)
+//      }
+//    )
+//  }
+
+  val page2 = Action.async { implicit request =>
+    Future.successful(Ok(uk.gov.hmrc.feedbacksurveyfrontend.views.html.awrsLookup.page2(formMappings.page2Form)))
+  }
+
+  val page2Continue = Action(parse.form(formMappings.page2Form)) { implicit request =>
+    Redirect(routes.AwrsLookupController.page3())
+  }
+
+  val page3 = Action.async { implicit request =>
+    Future.successful(Ok(uk.gov.hmrc.feedbacksurveyfrontend.views.html.awrsLookup.page3(formMappings.page3Form)))
+  }
+
+  val page3Continue = Action(parse.form(formMappings.page3Form)) { implicit request =>
+    Redirect(routes.AwrsLookupController.page4())
+  }
+
+  val page4 = Action.async { implicit request =>
+    Future.successful(Ok(uk.gov.hmrc.feedbacksurveyfrontend.views.html.awrsLookup.page4(formMappings.page4Form)))
+  }
+
+}
---
+++ app/uk/gov/hmrc/feedbacksurveyfrontend/models/AwrsLookupModels.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.awrsModels
+
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.Messages
+import play.api.libs.json.Json
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+
+case class Page1(ableToDoWhatNeeded: Option[String])
+
+object Page1 {
+  implicit val format = Json.format[Page1]
+}
+
+case class Page2(telephonedHmrc: Option[String],
+                  wroteToHmrc: Option[String],
+                  completedAnOnlineForm: Option[String],
+                  readGuidanceOnGovUk: Option[String],
+                  spokeToEmployerAgentOrAccountant: Option[String],
+                  spokeToAFriendOrFamilyMember: Option[String])
+
+object Page2 {
+  implicit val format = Json.format[Page2]
+}
+
+case class Page3(serviceReceived: Option[String])
+
+object Page3 {
+  implicit val format = Json.format[Page3]
+}
+
+case class Page4(recommendRating: Option[String],
+                  reasonForRating: Option[String])
+
+object Page4 {
+  implicit val format = Json.format[Page4]
+}
+
+case class AdditionalBusinessPremises(addAnother: Option[String])
+
+case class AdditionalBusinessPremisesList(premises: List[AdditionalBusinessPremises])
+
+
+object formMappings {
+
+  val page1Form = Form(mapping(
+    "ableToDoWhatNeeded" -> optional(text.verifying("required field", _.nonEmpty)))(Page1.apply)(Page1.unapply))
+
+  val page2Form = Form(mapping(
+    "telephonedHmrc" -> optional(text.verifying("required field", _.nonEmpty)),
+    "wroteToHmrc" -> optional(text.verifying("required field", _.nonEmpty)),
+    "completedAnOnlineForm" -> optional(text.verifying("required field", _.nonEmpty)),
+    "spokeToEmployerAgentOrAccountant" -> optional(text.verifying("required field", _.nonEmpty)),
+    "spokeToEmployerAgentOrAccountant" -> optional(text.verifying("required field", _.nonEmpty)),
+    "spokeToAFriendOrFamilyMember" -> optional(text.verifying("required field", _.nonEmpty)))(Page2.apply)(Page2.unapply))
+
+  val page3Form = Form(mapping(
+    "serviceReceived" -> optional(text.verifying("required field", _.nonEmpty)))(Page3.apply)(Page3.unapply))
+
+  val page4Form = Form(mapping(
+    "recommendRating" -> optional(text.verifying("required field", _.nonEmpty)),
+    "reasonForRating" -> optional(text.verifying("required field", _.nonEmpty)))(Page4.apply)(Page4.unapply))
+
+  def validInputCharacters(field: String, regXValue: String) = {
+    if (field.matches(regXValue)) true else false
+  }
+
+}
+
+object formFields {
+
+  val page2Question1Options = Seq(
+    "01" -> Messages("awrslookup.page2.question2.option1"),
+    "02" -> Messages("awrslookup.page2.question2.option2"),
+    "03" -> Messages("awrslookup.page2.question2.option3"),
+    "04" -> Messages("awrslookup.page2.question2.option4"),
+    "05" -> Messages("awrslookup.page2.question2.option5"),
+    "06" -> Messages("awrslookup.page2.question2.option6"),
+    "07" -> Messages("awrslookup.page2.question2.option7")
+  )
+
+  val page3Question1Options = Seq(
+    "01" -> Messages("awrslookup.page3.question1.option1"),
+    "02" -> Messages("awrslookup.page3.question1.option2"),
+    "03" -> Messages("awrslookup.page3.question1.option3"),
+    "04" -> Messages("awrslookup.page3.question1.option4"),
+    "05" -> Messages("awrslookup.page3.question1.option5")
+  )
+
+  val page4Question1Options = Seq(
+    "10" -> Messages("awrslookup.page4.question1.option1"),
+    "09" -> Messages("awrslookup.page4.question1.option2"),
+    "08" -> Messages("awrslookup.page4.question1.option3"),
+    "07" -> Messages("awrslookup.page4.question1.option4"),
+    "06" -> Messages("awrslookup.page4.question1.option5"),
+    "05" -> Messages("awrslookup.page4.question1.option6"),
+    "04" -> Messages("awrslookup.page4.question1.option7"),
+    "03" -> Messages("awrslookup.page4.question1.option8"),
+    "02" -> Messages("awrslookup.page4.question1.option9"),
+    "01" -> Messages("awrslookup.page4.question1.option10"),
+    "00" -> Messages("awrslookup.page4.question1.option11")
+  )
+
+}
+
+object fieldValidationPatterns {
+  def companyRegPattern = "(^[a-zA-Z0-9]{1,10})$"
+  def corporationRefPattern = "^([0-9]{10})$"
+  def addresssRegx = """^[A-Za-zÀ-ÿ0-9 &'(),-./]{0,}$"""
+  def postCodeRegx = """(GIR 0AA)|((([A-Z-[QVX]][0-9][0-9]?)|(([A-Z-[QVX]][A-Z-[IJZ]][0-9][0-9]?)|(([A-Z-[QVX‌​]][0-9][A-HJKSTUW])|([A-Z-[QVX]][A-Z-[IJZ]][0-9][ABEHMNPRVWXY]))))\s?[0-9][A-Z-[C‌​IKMOV]]{2})"""
+  def yesNoRegPattern = "^([1-2]{1})$"
+  def csvOdsRegPattern = "^((ods|csv))$"
+  def csvFileNameRegx = """^file[0-9]{1}$"""
+}
